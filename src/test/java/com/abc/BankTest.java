5fccfaf4d885087141988f40438a84c529fc2e22
Ashwani Bhadani <ash@Ashwanis-MacBook-Air.local>
2024-11-22 13:19:44 +0530

    FEAT: add total balance for account feature

--- src/test/java/com/abc/BankTest.java
+++ src/test/java/com/abc/BankTest.java
@@ -3,6 +3,7 @@ package com.abc;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class BankTest {
     private static final double DOUBLE_DELTA = 1e-15;
@@ -53,12 +54,30 @@ public class BankTest {
 
 
     @Test
-    public void super_savings_account() {
+    public void super_savings_account_when_deposit_greaterThan2000() {
         Bank bank = new Bank();
         Account superSavingsAccount = new Account(Account.SUPER_SAVINGS);
         bank.addCustomer(new Customer("Super Saver Bill").openAccount(superSavingsAccount));
         superSavingsAccount.deposit(3000.0);
         assertEquals(230.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void super_savings_account_when_deposit_greaterThan1000() {
+        Bank bank = new Bank();
+        Account superSavingsAccount = new Account(Account.SUPER_SAVINGS);
+        bank.addCustomer(new Customer("Super Saver Bill").openAccount(superSavingsAccount));
+        superSavingsAccount.deposit(1500.0);
+        assertEquals(75.0, bank.totalInterestPaid(), DOUBLE_DELTA);
+    }
+
+    @Test
+    public void super_savings_account_when_deposit_lesserThan1000() {
+        Bank bank = new Bank();
+        Account superSavingsAccount = new Account(Account.SUPER_SAVINGS);
+        bank.addCustomer(new Customer("Super Saver Bill").openAccount(superSavingsAccount));
+        superSavingsAccount.deposit(500.0);
+        assertEquals(20.0, bank.totalInterestPaid(), DOUBLE_DELTA);
     }
 
     @Test
@@ -75,6 +94,7 @@ public class BankTest {
         bank.addCustomer(johnDoe2);
 
         assertEquals(bank.getFirstCustomer(), "First Customer");
+        assertNotEquals(bank.getFirstCustomer(), "Second Customer");
     }
 
 }
